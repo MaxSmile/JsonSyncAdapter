783afb2e5cddae3f3544e1acde4a42abf098d5fe
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2015-08-22 13:17:30 +0300

    initial android proj

---
+++ JsonSyncAdapter/Application/src/main/java/com/vasilkoff/android/jsonsyncadapter/EntryListActivity.java
@@ -0,0 +1,16 @@
+package com.vasilkoff.android.jsonsyncadapter;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Activity for holding EntryListFragment.
+ */
+public class EntryListActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_list);
+    }
+}
